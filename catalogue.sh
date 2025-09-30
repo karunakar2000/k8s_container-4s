e4e07913a4d54a16ff34e496d8953814b74ad526
karunakar2000 <karunakar2000@gmail.com>
2025-09-30 20:57:03 +0200

    script-push

--- catalogue.sh
+++ catalogue.sh
@@ -8,6 +8,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_DIR=$PWD
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # This file path: /var/log/shell-script/16.log
 MONGODB_HOST=mongodb.myawsb60.xyz
 
