67338751cf7482a5a0a9f14d657eee8ecd66f340
karunakar2000 <karunakar2000@gmail.com>
2025-10-01 19:45:14 +0200

    script-Updated

--- set.sh
+++ set.sh
@@ -1,25 +1,28 @@
-# #!/bin/bash
-# set -e
-
-# error() {
-#     echo "there is an error in $LINENO, command is $BASH_COMMAND"
-# }  
-
-# trap error ERR
-
-# echo "HelloWorld"
-# echo "Before error"
-# MegaStar 2>/dev/null   # redirect stderr to /dev/null to hide Bash’s own message
-# echo "After error"
-
-
 #!/bin/bash
 set -e
 
-trap 'echo "there is an error in $LINENO, command is $BASH_COMMAND"' ERR
+error() {
+    echo "there is an error in $LINENO, command is $BASH_COMMAND"
+}  
+
+trap error ERR
 
 echo "HelloWorld"
 echo "Before error"
 MegaStar 2>/dev/null   # redirect stderr to /dev/null to hide Bash’s own message
 echo "After error"
+
+
+
+# Below Script is not using function and not recommended:
+
+#!/bin/bash
+# set -e
+
+# trap 'echo "there is an error in $LINENO, command is $BASH_COMMAND"' ERR
+
+# echo "HelloWorld"
+# echo "Before error"
+# MegaStar 2>/dev/null   # redirect stderr to /dev/null to hide Bash’s own message
+# echo "After error"
 
