8debf2d9ad8ff537cb92242ea00580b4f1dabae9
karunakar2000 <karunakar2000@gmail.com>
2025-10-01 19:15:13 +0200

    script-Updated

--- set.sh
+++ set.sh
@@ -1,14 +1,29 @@
+# #!/bin/bash
+# set -e
+
+# error(){
+# 	echo "there is an error in $LINENO, command is $BASH_COMMAND"
+
+# }  
+
+# trap error ERR
+
+# echo "HelloWorld"
+# echo "Before error"
+# sjhsjhhs
+# echo "After error"
+
+
 #!/bin/bash
 set -e
 
-error(){
-	echo "there is an error in $LINENO, command is $BASH_COMMAND"
-
+error() {
+    echo "there is an error in $LINENO, command is $BASH_COMMAND"
 }  
 
 trap error ERR
 
 echo "HelloWorld"
 echo "Before error"
-sjhsjhhs
+sjhsjhhs 2>/dev/null   # redirect stderr to /dev/null to hide Bash’s own message
 echo "After error"
